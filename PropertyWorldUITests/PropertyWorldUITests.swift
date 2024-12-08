14595a871d8dd9e4244e83baa728c3536c38724d
Abdullah Md. Zubair <raihan.zbr@gmail.com>
2024-12-08 12:57:19 +0100

    Project setup

---
+++ PropertyWorldUITests/PropertyWorldUITests.swift
@@ -0,0 +1,10 @@
+//
+//  PropertyWorldUITests.swift
+//  PropertyWorldUITests
+//
+//  Created by Abdullah Muhammad Zubair on 12/2/24.
+//
+
+import XCTest
+
+final class PropertyWorldUITests: XCTestCase {}
